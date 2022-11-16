8b938a96979446a09d8166784efcf75d56e9a4be
lukearcher <lukearcher1@gmail.com>
2022-11-16 15:45:41 +1100

    ready for deployment

--- sentiment_analysis/urls.py
+++ sentiment_analysis/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from sentiment import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('sentiment', views.sentiment, name='sentiment'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
